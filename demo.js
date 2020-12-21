18374952d7dc2cee12f4cf77293755d762886bf7
Tor Hagemann <tor.hagemann@socotra.com>
2020-12-21 04:12:06 -0700

    demo: make light (y) and sound (x/z) using baudio

--- demo.js
+++ demo.js
@@ -25,24 +25,48 @@ const selfTest = () => {
   a.play()
 }
 
-const handWave = async () => {
-  const rgb = (n, hue, s = 0.5, l = 0.5) => {
-    const k = (n + hue / 30) % 12
+const handWave = async (audio = true) => {
+  // https://en.wikipedia.org/wiki/Just_intonation
+  const just = (f, octaves = 2) => Math.pow(2, Math.floor(octaves * 12 * f) / 12)
+  const step = (hz, base = 440) => (12 * Math.log2(hz / base)).toFixed().padEnd(2)
+  const play = ([x, y, z], r = 250) => {
+    const fx = (r + x) / (2 * r)
+    const fz = (r + z) / (2 * r)
+    if (fx < 0 || fx > 1 || fz < 0 || fz > 1) return
+    const xhz = 440 * just(fx) // A=440 Hz (TODO: start C0?)
+    const zhz = 440 * just(fz) // FIXME: add relative minor?
+    log.debug('x+%smm => %s Hz [A440 +%s half tone(s)]', x, xhz, step(xhz))
+    log.debug('y+%smm (will be used for color)', y)
+    log.debug('z+%smm => %s Hz [A440 +%s half tone(s)]', z, zhz, step(zhz))
+    const a = b((t /*,intervals*/) => {
+      const ax = Math.sin(2 * Math.PI * t * xhz)
+      const az = Math.sin(2 * Math.PI * t * zhz)
+      return ax + az // sum of signals (two notes)
+    })
+    try {
+      a.play()
+    } catch (e) {
+      log.error(e)
+    }
+  }
+
+  const rgb = (k, s = 0.5, l = 0.5) => {
     const a = s * Math.min(l, 1 - l)
     const b = Math.min(k - 3, 9 - k)
     return l - a * Math.max(-1, Math.min(b, 1))
   }
   const toHexColor = ({ id, palmPosition, type }, min = 100, max = 400) => {
-    const y = Number(palmPosition[1]).toFixed(3)
+    const y = Number(palmPosition[1]).toFixed()
     log.debug('%s hand %s: +%smm', type, id, y)
     if (y < min || y > max) return '000000'
+    if (audio) play(palmPosition)
 
-    const hue = (y - min) / (max - min) // [0,1]
-    const r = Math.floor(0xff * rgb(0, 360 * hue))
-    const g = Math.floor(0xff * rgb(8, 360 * hue))
-    const b = Math.floor(0xff * rgb(4, 360 * hue))
+    const hue = 360 * ((y - min) / (max - min)) // in degrees
+    const r = Math.floor(0xff * rgb((0 + hue / 30) % 12))
+    const g = Math.floor(0xff * rgb((8 + hue / 30) % 12))
+    const b = Math.floor(0xff * rgb((4 + hue / 30) % 12))
     const hex = [r, g, b].map((c) => (c < 16 ? '0' : '') + c.toString(16)).join('')
-    log.debug('+%smm => #%s [hue %s]', y, hex, hue)
+    log.debug('y+%smm => #%s [hue:%s deg]', y, hex, hue.toFixed(2).padStart(7))
     return hex
   }
 
@@ -74,6 +98,7 @@ module.exports = {
 }
 
 if (!module.parent) {
-  selfTest()
-  handWave()
+  const audio = process.env.SOX !== '0'
+  selfTest() // ready
+  handWave(audio)
 }
